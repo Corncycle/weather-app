e7d0a30ff23ec2899d2dd55ea882f60ae176b305
Caleb Stromberg <calebstromberg@gmail.com>
2022-12-27 18:07:15 -0800

    Begin implementing input validation

--- src/index.js
+++ src/index.js
@@ -3,8 +3,35 @@ import './stylesheets/style.css'
 import { queryByCity } from './scripts/apiQuery'
 
 const btn = document.querySelector('.query-button')
-btn.addEventListener('click', (e) => {
-  queryByCity('Woodinville')
+const lousyBtn = document.querySelector('.lousy-query-button')
+const feelsElt = document.querySelector('.description')
+const tempElt = document.querySelector('.temperature')
+const locElt = document.querySelector('.location')
+
+btn.addEventListener('click', async (e) => {
+  const res = await queryByCity('Woodinville')
+  if (!res) {
+    feelsElt.innerText = 'Unknown'
+    tempElt.innerText = -1
+    locElt.innerText = 'Nowhere'
+    return
+  }
+  const [feels, temp, loc] = res
+  feelsElt.innerText = 'Feels like ' + feels
+  tempElt.innerText = temp
+  locElt.innerText = loc
 })
 
-console.log('hello world')
+lousyBtn.addEventListener('click', async (e) => {
+  const res = await queryByCity('safsdaf8$#%# 3j4%#$JKDSJFasf984')
+  if (!res) {
+    feelsElt.innerText = 'Unknown'
+    tempElt.innerText = -1
+    locElt.innerText = 'Nowhere'
+    return
+  }
+  const [feels, temp, loc] = res
+  feelsElt.innerText = 'Feels like ' + feels
+  tempElt.innerText = temp
+  locElt.innerText = loc
+})
--- src/scripts/apiQuery.js
+++ src/scripts/apiQuery.js
@@ -5,11 +5,29 @@ const k3 = '0e5b404eaf'
 const k = k1 + k2 + k3
 
 export async function queryByCity(city) {
+  if (!isValidCityName(city)) {
+    console.log('Nope, ' + city + ' is not a valid city name')
+    return false
+  }
+  console.log('Yep, ' + city + ' is a valid city name')
+  const resJson = await sendQueryByCity(city)
+  const temp = resJson.main.temp
+  const loc = resJson.name
+  const feels = resJson.main.feels_like
+  return [feels, temp, loc]
+}
+
+async function sendQueryByCity(city) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${k}&units=imperial`
   console.log('querying!')
   const response = await fetch(url, {
     mode: 'cors',
   })
-  const resJson = await response.json()
-  console.log(resJson)
+  return response.json()
+}
+
+// Lazily allow only letters and spaces
+// A more accurate city name parser would require a ton more effort for very little payoff
+function isValidCityName(s) {
+  return /^[a-z][a-z\s]*$/i.test(s)
 }
